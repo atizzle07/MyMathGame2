68ab278d6a5e19a5dd8c8886cc946da35d570346
Allen Tripp <13400103+atizzle07@users.noreply.github.com>
2025-11-26 22:18:29 -0600

    Inital Commit. Updated program file with some setup code.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+System.Console.Write("Welcome to the Math Game. Please enter your name: ");
+string? userName = Console.ReadLine();
+
+System.Console.WriteLine($"Thanks for playing, {userName}! Please select an option from the menu:");
+
+
+
+
+
+
