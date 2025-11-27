b30b2e3d0f62fb5c6bb2b82a9d9043b778dac062
Allen Tripp <13400103+atizzle07@users.noreply.github.com>
2025-11-27 00:28:30 -0600

    Added passable logic for remaining games. Still need to provide some error handling and number checking for division game.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿using System.Net.WebSockets;
-
-Console.Clear();
-System.Console.Write("Welcome to the Math Game. Please enter your name: ");
+﻿
+// Console.Clear();
+Console.Write("Welcome to the Math Game. Please enter your name: ");
 string? userName = Console.ReadLine();
 
-System.Console.WriteLine($"Thanks for playing, {userName}! Please select an option from the menu:");
+Console.WriteLine($"Thanks for playing, {userName}!\n");
 
 // Helper Variables
 bool gameActive = true;
@@ -13,19 +12,19 @@ int num1;
 int num2;
 Random random = new();
 int answer;
-int userScore;
+int userScore = 0;
 
 do // Main Game loop
 {
-    System.Console.WriteLine("Please make a selection:\n\n" +
+    Console.WriteLine("Please make a selection:\n\n" +
                 "[1]- Addition\n" +
                 "[2] - Subtraction\n" +
                 "[3] - Multiplication\n" +
                 "[4] - Division\n" +
                 "[Q] - Quit the game");
-    userInput = Console.ReadKey().ToString();
+    userInput = Console.ReadLine();
 
-    switch (userInput)
+    switch (userInput.ToLower())
     {
         case "1":
             AdditionGame();
@@ -39,13 +38,13 @@ do // Main Game loop
         case "4":
             DivisionGame();
             break;
-        case "Q":
-            System.Console.WriteLine("Exiting program... Press any key to proceed.");
+        case "q":
+            Console.WriteLine("Exiting program... Press any key to proceed.");
             Console.ReadKey();
             gameActive = false;
             break;
         default:
-            System.Console.WriteLine("That is not a valid selection. Please try again...");
+            Console.WriteLine("That is not a valid selection. Please try again...");
             break;
     }
 
@@ -57,45 +56,115 @@ void AdditionGame()
     int[] numbers = GetNumbers();
     answer = numbers[0] + numbers[1];
 
-    System.Console.WriteLine($"What is {numbers[0]} + {numbers[1]}?");
+    Console.WriteLine($"What is {numbers[0]} + {numbers[1]}?");
     userInput = Console.ReadLine();
     int.TryParse(userInput, out int userCheck);
 
+    //TODO - Need to introduce null or invalid error handling
+    //UPGRADE - Need to refactor this code into a single method shared between each game
     if (userCheck == answer)
     {
-        System.Console.WriteLine("That is correct!");
+        Console.WriteLine("That is correct!");
         userScore++;
     }
     else
     {
-        System.Console.WriteLine("Sorry, that answer is incorrect.");
+        Console.WriteLine("Sorry, that answer is incorrect.");
         if (userScore > 0) // Do not decrease score if it is already 0, which is the lowest valid score
         {
             userScore--;
         }
     }
-    //TODO - Need to introduce null or invalid error handling
+    
 }
 
 void SubtractGame()
 {
+    Console.Clear();
+    int[] numbers = GetNumbers(1,11);
+    answer = numbers[0] - numbers[1];
 
+    Console.WriteLine($"What is {numbers[0]} - {numbers[1]}?");
+    userInput = Console.ReadLine();
+    int.TryParse(userInput, out int userCheck);
+
+    //TODO - Need to introduce null or invalid error handling
+    //UPGRADE - Need to refactor this code into a single method shared between each game
+
+    if (userCheck == answer)
+    {
+        Console.WriteLine("That is correct!");
+        userScore++;
+    }
+    else
+    {
+        Console.WriteLine("Sorry, that answer is incorrect.");
+        if (userScore > 0) // Do not decrease score if it is already 0, which is the lowest valid score
+        {
+            userScore--;
+        }
+    }
 }
 
 void MultiplyGame()
 {
+    Console.Clear();
+    int[] numbers = GetNumbers(1,11);
+    answer = numbers[0] * numbers[1];
 
+    Console.WriteLine($"What is {numbers[0]} x {numbers[1]}?");
+    userInput = Console.ReadLine();
+    int.TryParse(userInput, out int userCheck);
+
+    //TODO - Need to introduce null or invalid error handling
+    //UPGRADE - Need to refactor this code into a single method shared between each game
+    if (userCheck == answer)
+    {
+        Console.WriteLine("That is correct!");
+        userScore++;
+    }
+    else
+    {
+        Console.WriteLine("Sorry, that answer is incorrect.");
+        if (userScore > 0) // Do not decrease score if it is already 0, which is the lowest valid score
+        {
+            userScore--;
+        }
+    }
 }
 
 void DivisionGame()
 {
+    Console.Clear();
+    int[] numbers = GetNumbers(1,11);
+    // TODO - Add logic to only proceed with the division game if the numbers produce an answer that is a whole number (int)
+    answer = numbers[0] / numbers[1];
 
+    Console.WriteLine($"What is {numbers[0]} / {numbers[1]}?");
+    userInput = Console.ReadLine();
+    int.TryParse(userInput, out int userCheck);
+
+    //TODO - Need to introduce null or invalid error handling
+    //UPGRADE - Need to refactor this code into a single method shared between each game
+    if (userCheck == answer)
+    {
+        Console.WriteLine("That is correct!");
+        userScore++;
+    }
+    else
+    {
+        Console.WriteLine("Sorry, that answer is incorrect.");
+        if (userScore > 0) // Do not decrease score if it is already 0, which is the lowest valid score
+        {
+            userScore--;
+        }
+    }
 }
 
-int[] GetNumbers()
+int[] GetNumbers(int minValue, int maxValue)
 {
-    num1 = random.Next(1, 10);
-    num2 = random.Next(1, 10);
+    num1 = random.Next(minValue, maxValue);
+    num2 = random.Next(minValue, maxValue);
 
     return [num1, num2];
 }
